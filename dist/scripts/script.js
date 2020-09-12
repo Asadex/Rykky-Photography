95ac69526373a7a79946d211f7a507e5ad35685e
Artur Malisevych <19artur98@gmail.com>
2020-09-12 18:01:13 +0200

    Done LIGHTBOX

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,22 +1,29 @@
-(function(){
     let $lightbox = $("#lightbox")
-    let $closerLightbox = $("#closerLightbox");
     let $openersLightbox = $(".openersLightbox")
+    let $closerLightbox = $("#closerLightbox");
     let galleryShow = false;
+    let $arrowRight = $(".arrowRight");
+    let $arrowLeft = $(".arrowLeft");
+    let $imageLightbox = $(".image-lightbox");
+    let slideIndex = 1;
 
-    $openersLightbox.click(openLightbox);
-    $closerLightbox.click(closeLightbox)
-
+    $openersLightbox.click(openLightbox)
+    $closerLightbox.click(closeLightbox);
+    
+    //OPENING AND CLOSING LIGHTBOX
     function openLightbox(){
         $lightbox.addClass("active")
         galleryShow = true;
         updateState();
     }
+
     function closeLightbox(){
         $lightbox.removeClass("active")
         galleryShow = false;
         updateState();
     }
+
+    //DISABLE SCROLL
     function updateState(){
         if(galleryShow){
             $('body').css({'overflow':'hidden'});
@@ -26,6 +33,16 @@
         }
     }
 
-    
+    //CURRENT IMAGE
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-})()
+    function showSlides(n) {
+        if(n==4){slideIndex = 5};
+        if(n>8){slideIndex=1};
+        $imageLightbox.attr("src",`dist/images/image-grid-${slideIndex}.jpg`)
+    }
