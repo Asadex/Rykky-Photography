e2d38ab84f29aa29e894ccb5435b8a8069fdf81c
Artur Malisevych <19artur98@gmail.com>
2020-09-12 12:19:15 +0200

    Lightbox opening closing + transition

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,3 +1,31 @@
+(function(){
+    let $lightbox = $("#lightbox")
+    let $closerLightbox = $("#closerLightbox");
+    let $openersLightbox = $(".openersLightbox")
+    let galleryShow = false;
 
+    $openersLightbox.click(openLightbox);
+    $closerLightbox.click(closeLightbox)
 
+    function openLightbox(){
+        $lightbox.addClass("active")
+        galleryShow = true;
+        updateState();
+    }
+    function closeLightbox(){
+        $lightbox.removeClass("active")
+        galleryShow = false;
+        updateState();
+    }
+    function updateState(){
+        if(galleryShow){
+            $('body').css({'overflow':'hidden'});
+        }else{
+            $(document).unbind('scroll'); 
+            $('body').css({'overflow':'visible'});
+        }
+    }
 
+    
+
+})()
