d7efeeb33c3a90d01b940c06f2c0a280f741f888
Artur Malisevych <19artur98@gmail.com>
2020-09-15 11:57:45 +0200

    Slider terminado + responsive

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,3 +1,4 @@
+    //LIGT BOX VARIABLES
     let $lightbox = $("#lightbox")
     let $openersLightbox = $(".openersLightbox")
     let $closerLightbox = $("#closerLightbox");
@@ -6,8 +7,34 @@
     let $arrowLeft = $(".arrowLeft");
     let $imageLightbox = $(".image-lightbox");
     let slideIndex = 1;
-    let serviceIndex = 1;
 
+    //SERVICE SLIDER
+    let serviceIndex = 0;
+    let $serviceTitle = $(".serviceTitle");
+    let $serviceDescription = $(".serviceDescription");
+    let $serviceImage = $(".service-image")
+    const services = [
+        {
+            image: "dist/images/studio.jpg",
+            title: "Estudio Profesional",
+            description: `<p class="lead">Disponibles luces profesionales, tenemos además una gran variedad de fondos para embellecer cualquier atuendo o escena.</p>
+            <p class="lead">También ofrecemos pantalla verde para hacer alguna escena con fondos profesionales, como si salieras en una película o colocarte en algún lugar que te gustaría visitar, en general toda clase de posibilidades para poder cumplir y sobrepasar tus expectativas.</p>`
+        },
+        {
+            image: "dist/images/reconstruccion.jpg",
+            title: "Departamento de reconstrucción",
+            description: `<p class="lead">Si tienes algunas fotos antiguas que te gustaría traer a la vida otra vez, tenemos solución para ti.</p>
+            <p class="lead">Esos recuerdos nublados ya no estarán en el olvido, todas las experiencia y memorias que has tenido volverán a tener el color y resplandor tal y como las recuerdas.</p>`
+        },
+        {
+            image: "dist/images/media.jpg",
+            title: "Consultas para redes sociales",
+            description: `<p class="lead">Consigue las últimas novedades y mejores consejos de parte de nuestro equipo con experiencia práctica en la industria.</p>
+            <p class="lead">Deja que nuestros asesores te guién al éxito en las redes sociales, asesoramiento de imagen personal, estilo de fotografía apropiada para tu perfil, mejores vías para conseguir la mayor cantidad de visualizaciones posibles, ideas de contenido para tu sector y mucha información más que consideremos necesaria para tu crecimiento.</p>`
+        },
+    ]
+
+    //ANIMATION VARIABLES
     let $animation_elements = $('.animation-element');
     let $window = $(window);
 
@@ -86,11 +113,35 @@
     }
     //SLIDER SERVICES
 
-    function nextService(){
+    function nextService(n){
         showService(serviceIndex += n);
     }
-    function showService(){
+    function showService(n){
+        if(n>2){serviceIndex = 0}
+        if(n<0){serviceIndex = 2}
 
+        $serviceImage.animate({
+           visibility:"hidden",
+           opacity:0,
+        })
+
+        // $serviceTitle.animate({
+        //     visibility:"hidden",
+        //     opacity:0,
+        // })
+        // $serviceDescription.animate({
+        //     visibility:"hidden",
+        //     opacity:0,
+        // })
+        //setTimeout(function(){
+            $serviceTitle.text(services[serviceIndex].title)
+            $serviceDescription.html(services[serviceIndex].description)
+        setTimeout(function(){
+            $serviceImage.attr("src",services[serviceIndex].image).animate({
+                visibility:"visible",
+                opacity:1,
+            })
+        },500)
     }
     //ANIMATIONS
     $(document).ready(function(){$(".hero-section").animate({
